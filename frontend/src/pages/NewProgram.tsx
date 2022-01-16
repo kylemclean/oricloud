1baed19ebb0f66f74756b2c1a53c06e5dc10f386
Kyle McLean <kylem590@gmail.com>
2022-01-16 07:47:50 -0700

    Uploading and downloading stuff works now

--- frontend/src/pages/NewProgram.tsx
+++ frontend/src/pages/NewProgram.tsx
@@ -8,16 +8,10 @@ import { useNavigate } from 'react-router-dom';
 
 interface NewProgramForm {
     name: string,
-    executable: File
+    executable: FileList
 }
 
 export default function NewProgram(): JSX.Element {
-    /*
-    const { programId } = useParams<{programId: string | undefined}>();
-
-    const [formPopulated, setFormPopulated] = useState<boolean>(false);
-    const [programError, setProgramError] = useState<string>('');*/
-
     const navigate = useNavigate();
 
     const { control, handleSubmit, register/*, reset*/ } = useForm<NewProgramForm>({
@@ -29,24 +23,8 @@ export default function NewProgram(): JSX.Element {
 
     const { isSubmitting } = useFormState({ control });
 
-    // Program editing code
-    /*
-    useEffect(() => {
-        if (programId) {
-            api.getProgram(programId)
-                .then(program => {
-                    reset(program);
-                    setFormPopulated(true);
-                }).catch(error => setProgramError(error.message));
-        } else {
-            setFormPopulated(true);
-        }
-        setFormPopulated(true);
-    }, []);
-    */
-
     const onSubmit: SubmitHandler<NewProgramForm> = data => {
-        api.createProgram(data)
+        api.createProgram({ executable: data.executable[0], name: data.name })
             .then(result => {
                 if (result.success) {
                     navigate(`/programs/${result.id}`);
@@ -57,60 +35,50 @@ export default function NewProgram(): JSX.Element {
     return (
         <Container maxWidth="md">
             <Typography variant="h3">
-                {/*programId ? "Edit Program" : "New Program"*/}
                 New Program
             </Typography>
-            {/*programError ? (
-                <Alert severity="error">
-                    <AlertTitle>Failed to get program</AlertTitle>
-                    {programError}
-                </Alert>
-            ) : formPopulated ? (*/
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {[
-                        <Controller
-                            name="name"
-                            control={control}
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    label="Name"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                />
-                            )}
-                        />,
-                        <>
-                            <Box>
-                                <label htmlFor="file-input">
-                                    Executable (*.wasm)
-                                </label>
-                            </Box>
-                            <input
-                                {...register("executable")}
-                                accept="application/wasm"
-                                id="file-input"
-                                type="file"
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {[
+                    <Controller
+                        name="name"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                label="Name"
+                                variant="outlined"
                                 required
+                                fullWidth
                             />
-                        </>,
-                        <LoadingButton
-                            type="submit"
-                            variant="contained"
-                            disabled={isSubmitting}
-                            loading={isSubmitting}
-                            startIcon={<CheckIcon />}
-                        >
-                            {/*programId ? "Save Program" : "Create Program"*/}
-                            Create Program
-                        </LoadingButton>].map((control, i) => (
-                            <Box key={i} sx={{ my: 2 }}>{control}</Box>
-                        ))}
-                </form>
-            /*) : (
-            <CircularProgress />
-            )*/}
-        </Container >
+                        )}
+                    />,
+                    <>
+                        <Box>
+                            <label htmlFor="file-input">
+                                Executable (*.wasm)
+                            </label>
+                        </Box>
+                        <input
+                            {...register("executable")}
+                            accept="application/wasm"
+                            id="file-input"
+                            type="file"
+                            required
+                        />
+                    </>,
+                    <LoadingButton
+                        type="submit"
+                        variant="contained"
+                        disabled={isSubmitting}
+                        loading={isSubmitting}
+                        startIcon={<CheckIcon />}
+                    >
+                        {/*programId ? "Save Program" : "Create Program"*/}
+                        Create Program
+                    </LoadingButton>].map((control, i) => (
+                        <Box key={i} sx={{ my: 2 }}>{control}</Box>
+                    ))}
+            </form>
+        </Container>
     );
 }
