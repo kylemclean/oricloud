d9a2880902a7586d88a073d36362421a99c70b03
Kyle McLean <kylem590@gmail.com>
2022-01-15 18:20:09 -0700

    Make database and models work

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,20 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
