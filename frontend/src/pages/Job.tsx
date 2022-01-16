afd8926920331d04a5b3843753d57fb6891a06c3
Kyle McLean <kylem590@gmail.com>
2022-01-16 07:12:51 -0700

    Add table of runs to job page

--- frontend/src/pages/Job.tsx
+++ frontend/src/pages/Job.tsx
@@ -1,14 +1,19 @@
-import { Alert, AlertTitle, CircularProgress, Container, Typography } from '@mui/material';
+import { Alert, AlertTitle, Box, CircularProgress, Container, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { Download as DownloadIcon } from '@mui/icons-material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../api/api';
 import { Job } from '../api/models/job';
+import { Run } from '../api/models/run';
 
 export default function JobPage(): JSX.Element {
     const { jobId } = useParams<{ jobId: string }>();
 
     const [job, setJob] = useState<Job | undefined>(undefined);
     const [jobError, setJobError] = useState<string>('');
+
+    const [runs, setRuns] = useState<Run[] | undefined>(undefined);
+    const [runsError, setRunsError] = useState<string>('');
 
     useEffect(() => {
         if (!jobId) {
@@ -18,15 +23,54 @@ export default function JobPage(): JSX.Element {
         api.getJob(jobId)
             .then(setJob)
             .catch(error => setJobError(error.message));
+        api.getRuns(jobId)
+            .then(setRuns)
+            .catch(error => setRunsError(error.message));
     }, [jobId]);
 
     return (
         <Container maxWidth="md">
-            {job ? (
+            {job ? <>
                 <Typography variant="h3">
                     {job.id}
                 </Typography>
-            ) : jobError ? (
+                <Box>
+                    <Typography variant="h4">
+                        Runs
+                    </Typography>
+                    <TableContainer component={Paper}>
+                        {runs ? (
+                            runs.length > 0 ? (
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Run ID</TableCell>
+                                            <TableCell>Output hash</TableCell>
+                                            <TableCell>Output file</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {runs.map(run => (
+                                            <TableRow key={run.id}>
+                                                <TableCell>{run.id}</TableCell>
+                                                <TableCell>...</TableCell>
+                                                <TableCell><IconButton aria-label="download"><DownloadIcon /></IconButton></TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            ) : "No runs yet."
+                        ) : runsError ? (
+                            <Alert severity="error">
+                                <AlertTitle>Failed to get runs</AlertTitle>
+                                {runsError}
+                            </Alert>
+                        ) : (
+                            <CircularProgress />
+                        )}
+                    </TableContainer>
+                </Box>
+            </> : jobError ? (
                 <Alert severity="error">
                     <AlertTitle>Failed to get job</AlertTitle>
                     {jobError}
