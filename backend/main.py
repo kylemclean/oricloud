63147df293e8acc594a491106185d3c067fde7da
mrcromero <mrcromero33@gmail.com>
2022-01-16 00:47:20 -0700

    removed hello world

--- backend/main.py
+++ backend/main.py
@@ -112,16 +112,6 @@ def complete_run(
     return result
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 def main():
     uvicorn.run("main:app", host="0.0.0.0", reload=True, debug=True, workers=3)
 
