49a4c2a60d5244c7b438bc61e8c71283a7b8cced
mrcromero <mrcromero33@gmail.com>
2022-01-15 21:37:07 -0700

    add job crud

---
+++ backend/crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+
+import models, schemas
+import uuid
+
+
+def get_jobs_from_user(db: Session, uid: str):
+    jobs = []
+    db_jobs = db.query(models.Job).filter(models.Job.creator_id == uid).all()
+    for db_job in db_jobs:
+        job_program = (
+            db.query(models.Program)
+            .filter(models.Program.id == db_job.program_id)
+            .first()
+        )
+        job = schemas.Job(id=db_job.id, state=db_job.state, program=job_program)
+        jobs.append(job)
+    return jobs
+
+
+def create_job(db: Session, pid, input):
+    input_msg = bytes(input, "utf-8")
+    # TODO: Get user id that created the job
+    user_id = 1
+    job = models.Job(
+        id=uuid.uuid4,
+        program_id=pid,
+        input=input_msg,
+        creator_id=user_id,
+        state="Pending",
+    )
+    db.add(job)
+    db.commit()
+    db.refresh(job)
+    return job
