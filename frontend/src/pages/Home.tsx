45767a7cabf1ba54d089ff122613f1340736c16a
Kyle McLean <kylem590@gmail.com>
2022-01-16 12:14:20 -0700

    Login working

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,97 +1,76 @@
 import React, { useState } from 'react';
-import { Alert, AlertTitle } from '@mui/material';
+import { Alert, AlertTitle, Button, Container, TextField, Typography } from '@mui/material';
+import api from '../api/api';
+import { useForm, useFormState, SubmitHandler, Controller } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { LoadingButton } from '@mui/lab';
 
-
-// This is a yikes, but I also don't know what I'm doing
-
-const setToken = (token: string) => {
-    const fetchUser = async () => {
-        const requestOptions = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token,
-            },
-        };
-
-        const response = await fetch("/jobs", requestOptions);
-        if (!response.ok) {
-            localStorage.setItem("authToken", "");
-        }
-        localStorage.setItem("authToken", token);
-    };
-    fetchUser()
-};
+interface LoginForm {
+    email: string,
+    password: string
+}
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+    const navigate = useNavigate();
 
-    const submitLogin = async () => {
-        const requestOptions = {
-            method: "POST",
-            headers: { "accept": "application/json", "Content-Type": "application/x-www-form-urlencoded" },
-            body: JSON.stringify(
-                `grant_type=&username=${email}&password=${password}&scope=&client_id=&client_secret=`
-            ),
-        };
+    const [error, setError] = useState<string>('');
 
-        const response = await fetch("/token", requestOptions);
-        const data = await response.json();
-
-        if (!response.ok) {
-            setError(data.detail);
-        } else {
-            setToken(data.access_token);
+    const { control, handleSubmit, register } = useForm<LoginForm>({
+        defaultValues: {
+            email: '',
+            password: ''
         }
+    });
+
+    const { isSubmitting } = useFormState({ control });
+
+    const onSubmit: SubmitHandler<LoginForm> = data => {
+        api.login(data.email, data.password)
+            .then(() => navigate('/programs'))
+            .catch((error) => setError(error.message));
     };
 
     return (
-        <div className="column">
-            <h1>Login Page</h1>
-            <form className="box" onSubmit={submitLogin}>
-                <h1 className="title has-text-centered">Login</h1>
-                <div className="field">
-                    <label className="label">Email Address</label>
-                    <div className="control">
-                        <input
-                            type="text"
-                            placeholder="Enter email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="input"
+        <Container maxWidth="sm">
+            <Typography variant="h3">Login</Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            label="Email"
+                            variant="outlined"
                             required
+                            fullWidth
                         />
-                    </div>
-                </div>
-                <div className="field">
-                    <label className="label">Password</label>
-                    <div className="control">
-                        <input
-                            type="password"
-                            placeholder="Enter password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="input"
+                    )}
+                />
+                <Controller
+                    name="password"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            label="Password"
+                            variant="outlined"
                             required
+                            fullWidth
                         />
-                    </div>
-                </div>
-                {error ? (
+                    )}
+                />
+                {error && (
                     <Alert severity="error">
-                        <AlertTitle>Username or password incorrect</AlertTitle>
+                        <AlertTitle>Email or password incorrect</AlertTitle>
                         {error}
                     </Alert>
-                ) : (
-                    <h2> Welcome </h2>
                 )}
-                <br />
-                <button className="button is-primary" type="submit">
+                <LoadingButton type="submit" disabled={isSubmitting} loading={isSubmitting} fullWidth variant="contained">
                     Login
-                </button>
+                </LoadingButton>
             </form>
-        </div>
+        </Container>
     );
 }
 
